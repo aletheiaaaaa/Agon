5c123fe7216f13eafd461362bf77a272f1ec0670
aletheiaaaaa <yasaswi.rallabandi@gmail.com>
2025-11-20 15:03:56 +0000

    second

--- include/agon/simd/simd.h
+++ include/agon/simd/simd.h
@@ -1,8 +1,7 @@
 #include <cstddef>
 #include <cstdint>
+#include <stdfloat>
 
 namespace agon::simd {
-    template<size_t N = 1>
-    void add_i8(const int8_t* src0, const int8_t* src1, int8_t* dst, size_t size);
-
+    
 }
